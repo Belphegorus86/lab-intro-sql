bcfaae8e8676f6af677ec37c3ac55f9e86a30980
Belphegorus86 <156254077+Belphegorus86@users.noreply.github.com>
2024-03-25 16:05:56 +0000

    Add files via upload

--- SQL LABS 1-9.sql
+++ SQL LABS 1-9.sql
@@ -25,3 +25,49 @@ select count(staff_id) from staff;
 select first_name from staff;
 
 # -> lab 2 
+
+# 1. Select all the actors with the first name ‘Scarlett’.
+select * from actor where first_name = "Scarlett";
+
+# 2. Select all the actors with the last name ‘Johansson’.        
+select * from actor where last_name = "Johansson";
+
+# 3. How many films (movies) are available for rent?
+select * from rental where return_date is NULL;
+select count(rental_id) from rental;
+select count(rental_id) from rental where return_date is NULL;
+
+# 4. How many films have been rented?
+select count(rental_id) from rental where return_date is not NULL;
+
+# 5. What is the shortest and longest rental period?
+select * from film;
+select min(rental_duration) as shortest_rental, max(rental_duration) as longest_rental from film;
+
+# 6. What are the shortest and longest movie duration? Name the values max_duration and min_duration.
+select * from film; 
+select min(length) as min_duration, max(length) as max_duration from film;
+
+# 7. What's the average movie duration?
+select avg(length) from film;
+
+# 8. What's the average movie duration expressed in format (hours, minutes)?
+SELECT 
+    CONCAT(
+        FLOOR(AVG(length) / 60), ' hours ',
+        AVG(length) % 60, ' minutes'
+    ) AS average_length 
+FROM 
+    film;
+    
+# 9. How many movies longer than 3 hours?
+select * from film;
+select count(*) from film where length > 180;
+
+# 10. Get the name and email formatted. Example: Mary SMITH - mary.smith@sakilacustomer.org.
+select concat(customer.first_name, ' ', (customer.last_name), ' - ', lower(customer.email)) as formatted from customer;
+
+# 11. What's the length of the longest film title?
+select MAX(length(title)) as longest_title from film;
+
+# -> lab 3  
